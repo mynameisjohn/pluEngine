004e09d13f7b957c7336b9cd1cc5a5f461e9bac9
John Joseph <johnjoseph@cakewalk.com>
2015-10-13 22:34:08 -0400

    Some skeleton code for the CenterFind phase

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
+#include "CenterFindEngine.h"
 
-int main() {
+int main(int argc, char ** argv) {
+	// Convert program args to CenterFind arguments
+	std::array<std::string, 12> args;
+
+	// Better be the right amount
+	if (argc < args.size() + 1)
+		return -1;
+
+	// Make them strings
+	for (int i=0; i<argc; i++)
+		args[i] = std::string(argv[i]);
+
+	// Construct parameters and engine
+	CenterFindEngine::Parameters params(args);
+	CenterFindEngine cF(params);
+
+	// Execute engine, get results
+	std::deque<CenterFindEngine::PMetricsVec> results = cF.Execute();
+
+	// Send this to the input of link3Dt (or whatever comes next)
+
 	return 0;
 }
